c84df8ae21267fb5a25c8438ca4b1f0cb09333b6
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-07-11 17:02:56 -0700

    added redirect if no session

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import { useSession } from '@supabase/auth-helpers-react'
+import { useRouter } from 'next/router';
+import { useEffect } from 'react'
 
 const Home = () => {
+  const session = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if(!session) {
+      console.log('you are not logged in')
+      router.push('/login')
+    }
+  }, []) 
+
   return (
     <div>Home</div>
   )
