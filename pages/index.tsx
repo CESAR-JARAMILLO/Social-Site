53e55f8bbfe832dd0f56d984839aff03000dcad1
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-07-11 18:39:28 -0700

    fixed user session bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,32 @@
 import { useSession } from '@supabase/auth-helpers-react'
 import { useRouter } from 'next/router';
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const Home = () => {
   const session = useSession()
   const router = useRouter()
 
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    if(!session) {
-      console.log('you are not logged in')
-      router.push('/login')
+    const checkUser = async () => {
+      const user = session?.user;
+
+      if (user) {
+        setLoading(false);
+      } else {
+        router.push('/login');
+      }
+    };
+
+    if (session) {
+      checkUser()
     }
-  }, []) 
+  }, [session]) 
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
 
   return (
     <div>Home</div>
