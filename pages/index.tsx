36710de0cfd1f29700958ef52cc72a9cb9030f99
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-07-11 19:14:40 -0700

    implimented getCurrentUser function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,43 @@
-import { useSession } from '@supabase/auth-helpers-react'
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react'
+import { signOut, getCurrentUser } from './api/auth';
 
 const Home = () => {
-  const session = useSession()
   const router = useRouter()
 
   const [loading, setLoading] = useState(true);
 
+  const handleLogout = async () => {
+    await signOut();
+    router.push('/login')
+  };
+
   useEffect(() => {
     const checkUser = async () => {
-      const user = session?.user;
+      const user = await getCurrentUser();
 
       if (user) {
         setLoading(false);
       } else {
+        setLoading(false);
         router.push('/login');
       }
     };
 
-    if (session) {
-      checkUser()
-    }
-  }, [session]) 
+    checkUser()
+  }, [])
 
   if (loading) {
     return <div>Loading...</div>
   }
 
   return (
-    <div>Home</div>
+    <>
+      <div>
+        Home
+      </div>
+      <button onClick={handleLogout}>Logout</button>
+    </>
   )
 }
 
