b78a0817d55e408f0c537ac9bd2928a10de51c2c
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-07-11 18:59:46 -0700

    Created signOut function

--- pages/api/auth.tsx
+++ pages/api/auth.tsx
@@ -39,3 +39,16 @@ export async function signIn(email: string, password: string) {
   }
 }
 
+export async function signOut() {
+  try {
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+      console.log('Error signing out:', error.message);
+    } else {
+      console.log('User signed out successfully');
+    }
+  } catch (error) {
+    console.error('Error signing out:', error);
+  }
+}
