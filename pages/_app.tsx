fd6fd08fe9f9cdd130c46cc592dc38ba30513fe9
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-07-11 18:42:20 -0700

    fixed supabase instance bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from 'next/app'
-import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
-import { useState } from 'react'
+import { supabase } from '@/lib/supabaseClient';
 
 export default function App({
   Component,
@@ -9,12 +8,10 @@ export default function App({
 }: AppProps<{
   initialSession: Session
 }>) {
-  // Create a new supabase browser client on every first render.
-  const [supabaseClient] = useState(() => createPagesBrowserClient())
 
   return (
     <SessionContextProvider
-      supabaseClient={supabaseClient}
+      supabaseClient={supabase}
       initialSession={pageProps.initialSession}
     >
       <Component {...pageProps} />
