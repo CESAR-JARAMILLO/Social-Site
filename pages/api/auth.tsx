2efa13fe672371035beee9791bed28ff1a27f3d6
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-07-11 18:40:09 -0700

    created signIn & signUp functions

---
+++ pages/api/auth.tsx
@@ -0,0 +1,41 @@
+import { supabase } from "@/lib/supabaseClient";
+
+export async function signUp(email: string, password: string): Promise<void> {
+  try {
+    const { data, error } =
+      await supabase.auth.signUp({
+        email: email,
+        password: password,
+      });
+
+    if (error) {
+      console.log("Error creating user:", error.message);
+    } else {
+      console.log("User created successfully:", data);
+    }
+  } catch (error) {
+    console.error("Error creating user:", error);
+  }
+}
+
+export async function signIn(email: string, password: string) {
+  try {
+    const { data, error } =
+      await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+      });
+
+    if (error) {
+      console.log("Error signing in:", error.message);
+      return { error };
+    } else {
+      console.log("Signed in successfully:", data);
+      return { data };
+    }
+  } catch (error) {
+    console.error("Error signing in:", error);
+    return { error };
+  }
+}
+
