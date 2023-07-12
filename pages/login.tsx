01268ade5f162988935b7514cdbcdf3ab1ef8d60
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-07-11 18:41:27 -0700

    removed unused code

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,65 @@
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { signIn } from './api/auth';
 
-export default Login
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter()
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const { data, error } = await signIn(email, password)
+
+    if (error) {
+      console.error('Error signing in:', error)
+    } else if (data) {
+      setEmail('');
+      setPassword('');
+      router.push('/')
+    }
+  };
+
+
+
+  return (
+    <>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Sign in</button>
+      </form>
+      <a href="/signup">Don't have an account?</a>
+    </>
+  );
+};
+
+export default Login;
