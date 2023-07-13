1366b5cc48f702e202bdcf4fec69759c564f462a
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-07-13 14:17:29 -0700

    implimented createPost

--- components/PostFormCard.tsx
+++ components/PostFormCard.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useState } from 'react';
+import { createPost } from '@/pages/api/postsAuth/postsAuth';
 
 const PostFormCard = () => {
+  const [text, setText] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+  
+    try {
+      const postData = await createPost(text);
+      console.log('Post created successfully:', postData);
+      alert(`You submitted: ${text}`);
+    } catch (error) {
+      console.error('Error creating post:', error);
+      alert('Error creating post. Please try again.')
+    }
+  };
+  
+
   return (
-    <div>PostFormCard</div>
+    <div>
+      <h3>PostFormCard</h3>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
 
-export default PostFormCard
+export default PostFormCard;
