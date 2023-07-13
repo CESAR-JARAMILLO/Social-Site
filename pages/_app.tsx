933f478f3aa2d76806e3704d1035cbecf819ce04
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-07-13 16:53:29 -0700

    setup ChakraProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
 import { supabase } from '@/lib/supabaseClient';
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({
   Component,
@@ -10,11 +11,13 @@ export default function App({
 }>) {
 
   return (
-    <SessionContextProvider
-      supabaseClient={supabase}
-      initialSession={pageProps.initialSession}
-    >
-      <Component {...pageProps} />
-    </SessionContextProvider>
+    <ChakraProvider>
+      <SessionContextProvider
+        supabaseClient={supabase}
+        initialSession={pageProps.initialSession}
+      >
+        <Component {...pageProps} />
+      </SessionContextProvider>
+    </ChakraProvider>
   )
 }
