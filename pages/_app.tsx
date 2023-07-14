7f28858fdc69557a2dbdb3da0df74b8e1c6eeb2e
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-07-14 16:16:29 -0700

    added Navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
 import { supabase } from '@/lib/supabaseClient';
 import { ChakraProvider } from '@chakra-ui/react'
+import Navbar from '@/components/Navbar';
 
 export default function App({
   Component,
@@ -16,6 +17,7 @@ export default function App({
         supabaseClient={supabase}
         initialSession={pageProps.initialSession}
       >
+        <Navbar />
         <Component {...pageProps} />
       </SessionContextProvider>
     </ChakraProvider>
