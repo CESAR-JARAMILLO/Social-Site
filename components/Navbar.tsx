2fd422843e509f1790ec5618e480bd5cdca895c3
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-07-14 16:16:04 -0700

    styled & added links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Box, IconButton, Flex, Link } from "@chakra-ui/react";
+import { FiHome, FiUser } from "react-icons/fi";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Flex justifyContent="space-around" alignItems="center" py={4} bg="blue.500">
+      <Link href="/" as="a">
+        <IconButton
+          aria-label="Home"
+          icon={<FiHome size={24} />}
+          color="white"
+          variant="ghost"
+        />
+      </Link>
 
-export default Navbar
+      <Link href="/account" as="a">
+        <IconButton
+          aria-label="Account"
+          icon={<FiUser size={24} />}
+          color="white"
+          variant="ghost"
+        />
+      </Link>
+    </Flex>
+  );
+};
+
+export default Navbar;
