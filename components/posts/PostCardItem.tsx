77c8d30fb52ff6b0bb49826290583a13729e8918
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-07-17 13:34:04 -0700

    feat: Add human-readable time difference for post creation time

--- components/posts/PostCardItem.tsx
+++ components/posts/PostCardItem.tsx
@@ -12,6 +12,7 @@ export type Post = {
   content: string;
   user_id: string;
   id: string;
+  created_at: any;
 }
 
 export type Like = {
@@ -30,6 +31,28 @@ export type PostCardItemProps = {
   setEditContent: React.Dispatch<React.SetStateAction<string>>;
   handleUpdate: (e: React.FormEvent) => void;
 }
+
+const getTimeDifference = (date: Date): string => {
+  const now = new Date();
+  const seconds = Math.floor((now.getTime() - date.getTime()) / 1000);
+
+  const intervals = [
+    { label: 'w', seconds: 604800 },
+    { label: 'd', seconds: 86400 },
+    { label: 'h', seconds: 3600 },
+    { label: 'm', seconds: 60 },
+    { label: 's', seconds: 1 },
+  ];
+
+  for (const interval of intervals) {
+    const count = Math.floor(seconds / interval.seconds);
+    if (count >= 1) {
+      return `${count}${interval.label}`;
+    }
+  }
+
+  return 'just now';
+};
 
 const PostCardItem: React.FC<PostCardItemProps> = ({ post, handleEdit, handleDelete, editPostId, setEditPostId, editContent, setEditContent, handleUpdate }) => {
   const [user, setUser] = useState<any>(null);
@@ -77,7 +100,6 @@ const PostCardItem: React.FC<PostCardItemProps> = ({ post, handleEdit, handleDel
       setLikes(prevLikes => prevLikes.filter(like => like.id !== userLike.id));
     }
   };
-  
 
   useEffect(() => {
     const fetchCommentsAndLikes = async () => {
@@ -86,12 +108,12 @@ const PostCardItem: React.FC<PostCardItemProps> = ({ post, handleEdit, handleDel
         const response = await getCommentsByPostId(post.id);
         if (response.error) throw response.error;
         setComments(prevComments => response.data || prevComments);
-  
+
         // Fetch likes
         const likesResponse = await getAllLikes();
         if (likesResponse.error) throw likesResponse.error;
         setLikes(prevLikes => likesResponse.data || prevLikes);
-  
+
         // Fetch user
         const userResponse = await getUserById(post.user_id);
         if (userResponse.error) throw userResponse.error;
@@ -100,10 +122,9 @@ const PostCardItem: React.FC<PostCardItemProps> = ({ post, handleEdit, handleDel
         console.log((error as Error).message);
       }
     };
-  
+
     fetchCommentsAndLikes();
   }, [post.id]);
-  
 
   const postLikes = likes.filter(like => like.post_id === post.id).length;
 
@@ -114,13 +135,15 @@ const PostCardItem: React.FC<PostCardItemProps> = ({ post, handleEdit, handleDel
       <Flex mb={4} gap={4}>
         <Avatar size="lg" src='/images/cesar.jpeg' />
         <Flex direction="column">
-        <Text fontSize="sm">
-        <Text as="span" fontWeight="bold">
-          {user?.full_name}
-          </Text>{" "}
-          shared a post
-        </Text>
-          <Text color="gray.500" fontSize="xs">4 hours ago</Text>
+          <Text fontSize="sm">
+            <Text as="span" fontWeight="bold">
+              {user?.full_name}
+            </Text>{" "}
+            shared a post
+          </Text>
+          <Text color="gray.500" fontSize="xs">
+            {getTimeDifference(new Date(post.created_at))}
+          </Text>
         </Flex>
         <Flex flex="1" justifyContent="flex-end">
           <Popover isOpen={isOpen} onOpen={onOpen} onClose={onClose}>
@@ -135,11 +158,27 @@ const PostCardItem: React.FC<PostCardItemProps> = ({ post, handleEdit, handleDel
             </PopoverTrigger>
             <PopoverContent mr={5}>
               <Flex direction="column" p="5" gap={2}>
-                <Flex as="button" onClick={() => { handleDelete(post.id); onClose(); }} p={4} _hover={handleButtonHover}>
+                <Flex
+                  as="button"
+                  onClick={() => {
+                    handleDelete(post.id);
+                    onClose();
+                  }}
+                  p={4}
+                  _hover={handleButtonHover}
+                >
                   <FiTrash2 size={24} />
                   <Text>Delete Post</Text>
                 </Flex>
-                <Flex as="button" onClick={() => { handleEdit(post.id, post.content); onClose(); }} p={4} _hover={handleButtonHover}>
+                <Flex
+                  as="button"
+                  onClick={() => {
+                    handleEdit(post.id, post.content);
+                    onClose();
+                  }}
+                  p={4}
+                  _hover={handleButtonHover}
+                >
                   <FiEdit size={24} />
                   <Text>Edit Post</Text>
                 </Flex>
@@ -155,7 +194,9 @@ const PostCardItem: React.FC<PostCardItemProps> = ({ post, handleEdit, handleDel
             onChange={e => setEditContent(e.target.value)}
             marginBottom="4"
           />
-          <Button colorScheme="blue" type="submit" mr="4">Update</Button>
+          <Button colorScheme="blue" type="submit" mr="4">
+            Update
+          </Button>
           <Button onClick={() => setEditPostId(null)}>Cancel</Button>
         </form>
       ) : (
@@ -163,21 +204,19 @@ const PostCardItem: React.FC<PostCardItemProps> = ({ post, handleEdit, handleDel
           <Text>{post.content}</Text>
           <Flex my={4} gap={10}>
             <Flex gap={2}>
-              <Box as='button' onClick={isLiked ? handleUnlikePost : handleLikePost}>
+              <Box as="button" onClick={isLiked ? handleUnlikePost : handleLikePost}>
                 <FiHeart size={20} color={isLiked ? 'red' : 'gray'} />
               </Box>
               <Text>{postLikes}</Text>
             </Flex>
             <Flex gap={2}>
-              <Box onClick={handleCommentsView} as='button'>
+              <Box onClick={handleCommentsView} as="button">
                 <FiMessageSquare size={20} />
               </Box>
               <Text>{comments?.length}</Text>
             </Flex>
           </Flex>
-          {viewComments && (
-            <Comments postId={post.id} />
-          )}
+          {viewComments && <Comments postId={post.id} />}
           <CommentForm postId={post.id} />
         </>
       )}
