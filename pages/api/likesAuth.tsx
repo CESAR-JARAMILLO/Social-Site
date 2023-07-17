00b409f14b284a8faf93d5b0fccd9630875c7f49
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-07-16 23:29:10 -0700

    created createLike function

--- pages/api/likesAuth.tsx
+++ pages/api/likesAuth.tsx
@@ -4,6 +4,16 @@ export async function getAllLikes() {
   const { data, error } = await supabase
     .from('likes')
     .select('*');
+
+  return { data, error };
+}
+
+export async function createLike(user_id: string, post_id: string) {
+  const { data, error } = await supabase
+    .from('likes')
+    .insert([
+      { user_id: user_id, post_id: post_id }
+    ]);
   
   return { data, error };
 }
