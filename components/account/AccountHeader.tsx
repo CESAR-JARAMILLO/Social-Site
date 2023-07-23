a94d39859bb03aa81c77dcd9df600f0305d5c48c
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-07-23 09:47:07 -0700

    added bottom margin

--- components/account/AccountHeader.tsx
+++ components/account/AccountHeader.tsx
@@ -11,7 +11,7 @@ const AccountHeader: React.FC<AccountHeaderProps> = ({ setActiveComponent }) => 
   const [isLargerThanMD] = useMediaQuery("(min-width: 768px)");
 
   return (
-    <Flex direction="column" justifyContent="space-between" bg="white" w={isLargerThanMD ? "80%" : "100%"} m={isLargerThanMD ? "40px auto" : "none"} borderRadius={isLargerThanMD ? "lg" : "none"} overflow="hidden">
+    <Flex mb={8} direction="column" justifyContent="space-between" bg="white" w={isLargerThanMD ? "80%" : "100%"} m={isLargerThanMD ? "40px auto" : "none"} borderRadius={isLargerThanMD ? "lg" : "none"} overflow="hidden">
       <Box h="200px" bgImage="url('https://source.unsplash.com/random')" bgSize="cover" position="relative">
         <Icon as={FiCamera} p={1} w={8} h={8} position="absolute" bottom={4} right={6} color="white" bg="black" borderRadius={10}/>
       </Box>
