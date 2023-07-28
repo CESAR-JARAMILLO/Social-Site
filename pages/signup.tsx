bca6d6f9f553f2bd2d8d50f52ef856ecd9470c50
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-07-28 13:28:17 -0700

    deleted SignupStepper component

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,14 +1,10 @@
 import { Box, Flex } from '@chakra-ui/react';
-import SignupStepper from '@/components/signup/SignupStepper';
 import SignupForm from '@/components/signup/SignupForm';
 
 const Signup = () => {
 
   return (
     <Box minHeight="100vh" mt={10}>
-      <Flex justifyContent="center" alignItems="center">
-        <SignupStepper />
-      </Flex>
       <Flex mt="6em" alignItems="center" justifyContent="center">
         <SignupForm />
       </Flex>
