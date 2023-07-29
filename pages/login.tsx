406dc2875b69d2fb057acd0c69d933da02d74fe1
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-07-28 23:21:06 -0700

    implimented chakra Alert

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,13 @@
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { signIn } from './api/auth';
-import { Box, Button, Flex, Input, Link, useBreakpointValue, Heading } from '@chakra-ui/react';
+import { Box, Button, Flex, Input, Link, useBreakpointValue, Heading, Alert, AlertIcon } from '@chakra-ui/react';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | boolean>(false);
   const router = useRouter();
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -18,23 +20,39 @@ const Login = () => {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
-    const { data, error } = await signIn(email, password);
-
+  
+    const { error } = await signIn(email, password);
+  
     if (error) {
-      console.error('Error signing in:', error)
-    } else if (data) {
-      setEmail('');
-      setPassword('');
-      router.push('/')
+      setErrorMessage(`Failed to sign in: ${error}`);
+      return;
     }
+  
+    setErrorMessage(null)
+    setSuccessMessage(true)
+    setEmail('');
+    setPassword('');
+    router.push('/')
   };
+  
 
   const formWidth = useBreakpointValue({ base: "90%", md: "60%", lg: "40%" });
 
   return (
     <Flex minHeight="100vh" alignItems="center" justifyContent="center">
       <Box width={formWidth}>
+      {errorMessage && (
+          <Alert borderRadius={20} mb={5} status='error'>
+            <AlertIcon />
+            {errorMessage}
+          </Alert>
+        )}
+        {successMessage && (
+          <Alert borderRadius={20} mb={5} status='success'>
+            <AlertIcon />
+            Login successfull!
+          </Alert>
+        )}
       <Heading textAlign="center" marginBottom="2em">Login</Heading>
         <form onSubmit={handleSubmit}>
           <Flex direction="column" marginBottom="1em">
