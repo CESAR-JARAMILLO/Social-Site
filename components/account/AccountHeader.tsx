f5f1488e5f7533c6e2bef1e1257f59f271119b78
Cesar Jaramillo <31831282+CESAR-JARAMILLO@users.noreply.github.com>
2023-08-03 21:07:13 -0700

    fixed bugs

--- components/account/AccountHeader.tsx
+++ components/account/AccountHeader.tsx
@@ -6,133 +6,90 @@ import { v4 as uuidv4 } from 'uuid';
 
 interface AccountHeaderProps {
   setActiveComponent: (componentName: string) => void;
-  url: string | null;
+  avatarUrl: string | null;
+  backgroundUrl: string | null;
 }
 
-const AccountHeader: React.FC<AccountHeaderProps> = ({ setActiveComponent, url}) => {
-  const [avatarUrl, setAvatarUrl] = useState<string | null>(null)
-  const [backgroundUrl, setBackgroundUrl] = useState<string | null>(null)
+const AccountHeader: React.FC<AccountHeaderProps> = ({ setActiveComponent, avatarUrl, backgroundUrl }) => {
   const [uploading, setUploading] = useState(false)
+  const [localAvatarUrl, setAvatarUrl] = useState<string | null>(null)
+  const [localBackgroundUrl, setBackgroundUrl] = useState<string | null>(null)
   const user = useUser()
   const supabase = useSupabaseClient()
-  const fileInputRef = useRef<HTMLInputElement>(null);
-  const fileInputRefBackground = useRef<HTMLInputElement>(null);
+  const avatarInputRef = useRef<HTMLInputElement>(null);
+  const backgroundInputRef = useRef<HTMLInputElement>(null);
   const [isLargerThanMD] = useMediaQuery("(min-width: 768px)");
 
   useEffect(() => {
-    if (url) setAvatarUrl(url)
-  }, [url])
+    setAvatarUrl(avatarUrl)
+    setBackgroundUrl(backgroundUrl)
+  }, [avatarUrl, backgroundUrl])
 
 
-  async function uploadBackgroundImage(event: React.ChangeEvent<HTMLInputElement>) {
+  async function uploadFile(event: React.ChangeEvent<HTMLInputElement>, isAvatar: boolean) {
     try {
-        setUploading(true)
+      setUploading(true)
 
-        const files: FileList | null = event.target.files;
-        if (files && files[0]) {
-          const file = files[0];
+      const files: FileList | null = event.target.files;
+      if (files && files[0]) {
+        const file = files[0];
+        const filePath = `${user?.id}/${uuidv4()}`;
 
-          const uploadResponse = await supabase
-            .storage
-            .from('background_images')
-            .upload(user?.id + "/" + uuidv4(), file);
+        const uploadResponse = await supabase
+          .storage
+          .from(isAvatar ? 'avatars' : 'backgrounds')
+          .upload(filePath, file);
 
-          if (uploadResponse.data) {
-            const backgroundPublicUrl = process.env.NEXT_PUBLIC_SUPABASE_URL + '/storage/v1/object/public/background_images/' + uploadResponse.data.path;
+        if (uploadResponse.data) {
+          const filePublicUrl = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/${isAvatar ? 'avatars' : 'backgrounds'}/${filePath}`;
 
-            if (user) {
-                const { error } = await supabase
-                .from('profiles')
-                .upsert({
-                    id: user.id,
-                    background_url: backgroundPublicUrl,
-                })
+          if (user) {
+            const { error } = await supabase
+              .from('profiles')
+              .upsert({
+                id: user.id,
+                [isAvatar ? 'avatar_url' : 'background_url']: filePublicUrl,
+              })
 
-                setBackgroundUrl(backgroundPublicUrl)
-
-                if (error) {
-                    throw error
-                }
+            if (isAvatar) {
+              setAvatarUrl(filePublicUrl)
+            } else {
+              setBackgroundUrl(filePublicUrl)
             }
-          } else {
-            throw new Error('Upload failed');
-          }
-        }
-      } catch (error: unknown) {
-        if (error instanceof Error) {
-          if (error.message === 'Upload failed') {
-            alert('Failed to upload the image. Please try again.')
-          } else {
-            alert(`Unexpected error: ${error.message}`)
+
+            if (error) {
+              throw error
+            }
           }
         } else {
-          console.log(error);
-        }         
-      } finally {
-        setUploading(false)
-        if (fileInputRefBackground.current) {
-          fileInputRefBackground.current.value = '';
+          throw new Error('Upload failed');
         }
       }
-    }
-
-
-  async function uploadAvatar(event: React.ChangeEvent<HTMLInputElement>) {
-    try {
-        setUploading(true)
-
-        const files: FileList | null = event.target.files;
-        if (files && files[0]) {
-          const file = files[0];
-
-          const uploadResponse = await supabase
-            .storage
-            .from('avatars')
-            .upload(user?.id + "/" + uuidv4(), file);
-
-          if (uploadResponse.data) {
-            const avatarPublicUrl = process.env.NEXT_PUBLIC_SUPABASE_URL + '/storage/v1/object/public/avatars/' + uploadResponse.data.path;
-
-            if (user) {
-                const { error } = await supabase
-                .from('profiles')
-                .upsert({
-                    id: user.id,
-                    avatar_url: avatarPublicUrl,
-                })
-
-                setAvatarUrl(avatarPublicUrl)
-
-                if (error) {
-                    throw error
-                }
-            }
-          } else {
-            throw new Error('Upload failed');
-          }
-        }
-      } catch (error: unknown) {
-        if (error instanceof Error) {
-          if (error.message === 'Upload failed') {
-            alert('Failed to upload the image. Please try again.')
-          } else {
-            alert(`Unexpected error: ${error.message}`)
-          }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        if (error.message === 'Upload failed') {
+          alert('Failed to upload the image. Please try again.')
         } else {
-          console.log(error);
-        }         
-      } finally {
-        setUploading(false)
-        if (fileInputRef.current) {
-          fileInputRef.current.value = '';
+          alert(`Unexpected error: ${error.message}`)
         }
+      } else {
+        console.log(error);
+      }
+    } finally {
+      setUploading(false)
+      if (avatarInputRef.current) {
+        avatarInputRef.current.value = '';
+      }
+      if (backgroundInputRef.current) {
+        backgroundInputRef.current.value = '';
       }
     }
+  }
 
   return (
     uploading ? <Spinner /> :
     <Flex mb={8} direction="column" justifyContent="space-between" bg="white" w={isLargerThanMD ? "80%" : "100%"} m={isLargerThanMD ? "40px auto" : "none"} borderRadius={isLargerThanMD ? "lg" : "none"} overflow="hidden">
-      <Box h="200px" bgImage={`url(${backgroundUrl || 'https://source.unsplash.com/random'})`} bgSize="cover" position="relative">
+      <Box h="200px" bgImage={`url(${localBackgroundUrl})`} bgSize="cover" position="relative">
       <Box 
         bg="gray.300"
         p={2}
@@ -141,30 +98,30 @@ const AccountHeader: React.FC<AccountHeaderProps> = ({ setActiveComponent, url})
         position="absolute"
         bottom={4}
         right={6}
-        onClick={() => fileInputRefBackground.current?.click()}
+        onClick={() => backgroundInputRef.current?.click()}
       >
         <FiCamera size={22} color="white" fill="black" />
       </Box>
-      <input
-        type="file"
-        accept="image/*"
-        style={{ display: 'none' }}
-        ref={fileInputRefBackground}
-        onChange={uploadBackgroundImage}
-      />
+        <input
+          type="file"
+          accept="image/*"
+          style={{ display: 'none' }}
+          ref={backgroundInputRef}
+          onChange={(event) => uploadFile(event, false)}
+        />
       </Box>
       <Flex direction="column" align={isLargerThanMD ? "center" : "flex-start"} ml={isLargerThanMD ? "none" : 6}>
         <Box position="relative">
           <Avatar 
             name="Cesar Jaramillo"
-            src={avatarUrl || undefined}
+            src={localAvatarUrl || undefined}
             boxSize="100px"
             position="relative"
             top="-50px"
             border="2px solid white"
             bg="white"
           />
-          <Box bg="gray.300" p={2} borderRadius={20} border="1px solid white" position="absolute" bottom={14} right={-1} onClick={() => fileInputRef.current?.click()}>
+          <Box bg="gray.300" p={2} borderRadius={20} border="1px solid white" position="absolute" bottom={14} right={-1} onClick={() => avatarInputRef.current?.click()}>
           <FiCamera size={20} color="white" fill="black" />
         </Box>
         </Box>
