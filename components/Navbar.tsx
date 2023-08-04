b266fd09bc29b612586617a45d91b4b0e199f82d
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-08-04 00:02:12 -0700

    added logout icon and function

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Box, IconButton, Flex, Link } from "@chakra-ui/react";
-import { FiHome, FiUser } from "react-icons/fi";
+import { FiHome, FiUser, FiLogOut, FiLogIn } from "react-icons/fi";
+import { signOut } from '@/pages/api/auth';
+import { useRouter } from 'next/router';
+import { useUser } from '@supabase/auth-helpers-react';
 
 const Navbar = () => {
+  const router = useRouter()
+  const user = useUser()
+
+  const handleLogout = async () => {
+    await signOut();
+    router.push('/login')
+  };
+
   return (
     <Flex justifyContent="space-around" alignItems="center" py={4} bg="blue.500">
       <Link href="/" as="a">
@@ -22,6 +33,28 @@ const Navbar = () => {
           variant="ghost"
         />
       </Link>
+      
+      <>
+        {user ? (
+          <Link as="a" onClick={handleLogout}>
+          <IconButton
+            aria-label="Account"
+            icon={<FiLogOut size={24} />}
+            color="white"
+            variant="ghost"
+          />
+        </Link>
+        ) : (
+          <Link as="a" href='/'>
+            <IconButton
+              aria-label="Account"
+              icon={<FiLogIn size={24} />}
+              color="white"
+              variant="ghost"
+            />
+          </Link>
+        )}
+      </>
     </Flex>
   );
 };
