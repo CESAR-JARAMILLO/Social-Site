b4b420fe2a32cc906314c84b4afe442068196f9f
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-08-03 19:56:07 -0700

    refactored

--- pages/account.tsx
+++ pages/account.tsx
@@ -7,14 +7,15 @@ import { useState, useEffect } from 'react';
 import { useSessionContext } from '@supabase/auth-helpers-react';
 import { useRouter } from 'next/router';
 import { getCurrentUserProfile } from './api/auth';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Spinner } from '@chakra-ui/react';
 
 const Account = () => {
   const [activeComponent, setActiveComponent] = useState('posts');
-  const [completedSignup, setCompletedSignup] = useState(null)
+  const [completedSignup, setCompletedSignup] = useState(null);
+  const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
   const { isLoading, session } = useSessionContext();
-  const [userId, setUserId] = useState('')
-  const router = useRouter()
+  const [userId, setUserId] = useState('');
+  const router = useRouter();
 
   useEffect(() => {
     const fetchUserProfile = async () => {
@@ -25,7 +26,8 @@ const Account = () => {
           const currentUserProfile = await getCurrentUserProfile();
           if (currentUserProfile) {
             setCompletedSignup(currentUserProfile[0]?.completed_signup);
-            setUserId(currentUserProfile[0]?.id)
+            setUserId(currentUserProfile[0]?.id);
+            setAvatarUrl(currentUserProfile[0]?.avatar_url);
           } else {
             console.error("User profile is undefined.");
           }
@@ -39,14 +41,17 @@ const Account = () => {
   }, [isLoading, session, router]);  
 
   if (isLoading) {
-    return <div>Loading...</div>
+    return <Spinner />
   }
 
   return (
     <>
       {completedSignup ? (
         <Box minHeight="100vh" mt={10}>
-          <AccountHeader setActiveComponent={setActiveComponent} />
+          <AccountHeader 
+            setActiveComponent={setActiveComponent} 
+            url={avatarUrl} 
+          />
           {activeComponent === 'posts' && <AccountPostCard />}
           {activeComponent === 'images' && (
             <Flex width="100%" justifyContent="center">
@@ -58,7 +63,8 @@ const Account = () => {
           {activeComponent === 'edit' &&
             <Flex mt={6} alignItems="center" justifyContent="center">
               <AccountEditProfile />
-            </Flex>}
+            </Flex>
+          }
         </Box>
       ) : (
         <Box minHeight="100vh" mt={10}>
